d87d2e135f30950d91ec606f9b6f9bcf952f084d
Chadwick <chadwick.employment@gmail.com>
2025-01-03 16:42:21 +0100

    get more script like

--- get_checkins.py
+++ get_checkins.py
@@ -15,24 +15,9 @@ import requests
 
 from untappd_cred import cred
 
-pagina = 1
-checkins_log = "checkins.log"
-checkins_file = "checkins.json"
-checkins = dict()
-checkins_access_token: str = cred["checkins_access_token"]
-checkins_url: str = (
-    "https://api.untappd.com/v4/user/checkins?"
-    f"access_token={checkins_access_token}"
-)
-cred_add = (
-    f"&access_token={cred['checkins_access_token']}"
-    f"&client_id={cred['client_id']}"
-    f"&client_secret={cred['client_secret']}"
-)
 
-
-def get_checkins(url):
-    response = requests.get(
+def get_checkins_response(url):
+    return requests.get(
         url,
         headers={
             "Content-Type": "application/json",
@@ -40,37 +25,58 @@ def get_checkins(url):
         }
     )
 
-    return response
 
+def get_checkins():
+    pagina = 1
+    checkins_log = "checkins.log"
+    checkins_file = "checkins.json"
+    checkins_access_token: str = cred["checkins_access_token"]
+    checkins_url: str = (
+        "https://api.untappd.com/v4/user/checkins?"
+        f"access_token={checkins_access_token}"
+    )
+    cred_add = (
+        f"&access_token={cred['checkins_access_token']}"
+        f"&client_id={cred['client_id']}"
+        f"&client_secret={cred['client_secret']}"
+    )
 
-checkins_response = get_checkins(checkins_url)
-checkins_json = checkins_response.json()
-with open(f"checkins_{pagina:02d}.log", "w") as clph:
-    json.dump(checkins_json, clph, indent=4)
-checkins.update(checkins_json)
-
-if checkins_json["meta"]["code"] == 200:
-    while checkins_json["response"]["pagination"].get("next_url"):
-        nu = checkins["response"]["pagination"]["next_url"]
-        print(f"next_url: {nu}")
-        pagina += 1
-        print(f"pagina: {pagina:02d}")
-        next_url = (
-            f"{checkins_json['response']['pagination']['next_url']}"
-            f"{cred_add}"
-        )
-        checkins_response = get_checkins(next_url)
-        checkins_json = checkins_response.json()
+    checkins_response = get_checkins_response(checkins_url)
+    checkins_json = checkins_response.json()
+    if checkins_json["meta"]["code"] == 200:
         with open(f"checkins_{pagina:02d}.log", "w") as clph:
             json.dump(checkins_json, clph, indent=4)
-        checkins.update(checkins_json)
-        if nu == "" or nu is None:
-            break
 
-    with open(checkins_log, "w") as clh:
-        clh.write(f"{checkins}")
+        checkins = checkins_json
 
-    with open(checkins_file, "w") as cfh:
-        json.dump(checkins, cfh, indent=4)
-else:
-    print(f"non 200 response: {checkins_json['meta']}")
+        while checkins_json["response"]["pagination"].get("next_url"):
+            nu = checkins["response"]["pagination"]["next_url"]
+            print(f"next_url: {nu}")
+            pagina += 1
+            print(f"pagina: {pagina:02d}")
+            next_url = (f"{nu}{cred_add}")
+            checkins_response = get_checkins_response(next_url)
+            checkins_json = checkins_response.json()
+            with open(f"checkins_{pagina:02d}.log", "w") as clph:
+                json.dump(checkins_json, clph, indent=4)
+            checkins.update(checkins_json)
+            if nu == "" or nu is None:
+                break
+            if pagina > 2:
+                break
+
+        with open(checkins_log, "w") as clh:
+            clh.write(f"{checkins}")
+
+        with open(checkins_file, "w") as cfh:
+            json.dump(checkins, cfh, indent=4)
+    else:
+        print(f"non 200 response: {checkins_json['meta']}")
+
+
+def main():
+    get_checkins()
+
+
+if __name__ == "__main__":
+    main()
