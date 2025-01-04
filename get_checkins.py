dddb1a2f47a8f97a38df38c1e72c10a079e3fd90
Chadwick <chadwick.employment@gmail.com>
2025-01-04 22:30:17 +0100

    refactor to get_checkins_json

--- get_checkins.py
+++ get_checkins.py
@@ -1,19 +1,33 @@
 # TODO:
-# mock the untapd server with fastapi
-# use click to ask to continue during while loop
-# backup checkins_file
-# read in checkins_file and get latest checkin_id
-# during while loop
-#    break if latest checkin_id found
-#    ask if continue or break
-# update checkins dict
-# write checkins dict to new checkins_file
-# put it all in sqlite
+# [P] mock the untapd server with fastapi
+# [ ] use typer to ask to continue during while loop
+# [X] backup checkins_file
+# [X] read in checkins_file and get latest checkin_id
+# [ ] during while loop
+# [ ]    break if latest checkin_id found
+# [ ]    ask if continue or break
+# [X] update checkins dict (now a list)
+# [ ] write checkins dict to new checkins_file
+# [ ] put it all in sqlite
 
+import time
 import json
 import requests
+from datetime import datetime
+from pathlib import Path
 
 from untappd_cred import cred
+
+
+RATE_THRESHOLD = 5
+
+
+def Non200(Exception):
+    pass
+
+
+def RateLimitExceeded(Exception):
+    pass
 
 
 def get_checkins_response(url):
@@ -22,12 +36,59 @@ def get_checkins_response(url):
         headers={
             "Content-Type": "application/json",
             "Origin": "https://freeshell.de",
+            "User-Agent": f"Simplified {cred['client_id']}",
         }
     )
 
 
+def get_checkins_json(checkins_url, pagina, checkins, just_checkins):
+    print(f"url: {checkins_url}")
+    print(f"pagina: {pagina:02d}")
+    checkins_response = get_checkins_response(checkins_url)
+    rate_limit = checkins_response.headers["x-ratelimit-limit"]
+    rate_remain = checkins_response.headers["x-ratelimit-remaining"]
+    print(f"{rate_remain} requests left of {rate_limit} this hour")
+    if int(rate_remain) < RATE_THRESHOLD:
+        print(f"WARNING {rate_remain} requests left of {rate_limit} this hour")
+    if int(rate_remain) == 0:
+        raise RateLimitExceeded(
+            "ERROR rate_limit exceeded {rate_remain} of {rate_limit}"
+        )
+    # if DEBUG or something ?
+    # Path("checkins_re_headers").write_text(f"{checkins_re_headers}")
+    checkins_json = checkins_response.json()
+    meta_code = checkins_json["meta"]["code"]
+    if meta_code != 200:
+        raise Non200(f"ERROR meta code not 200: {meta_code}")
+    checkins_items = checkins_json["response"]["checkins"]["items"]
+    Path(f"db/checkins_{pagina:02d}.json").write_text(
+        json.dumps(checkins_json, indent=4)
+    )
+    Path(f"db/just_checkins_{pagina:02d}.json").write_text(
+        json.dumps(checkins_items, indent=4)
+    )
+    just_checkins.append(checkins_items)
+    checkins.append(checkins_json)
+
+    return checkins, checkins_json, just_checkins
+
+
 def get_checkins():
+    found_last_db_id = False
+    last_db_id = 0
+    db_path = Path("db")
+    if db_path.exists():
+        db_path.rename(f"db_{datetime.now().strftime('%F')}")
+    else:
+        db_path.mkdir()
+    if db_files := sorted([f for f in db_path.iterdir()]):
+        last_checkins = json.loads(db_files[0].read_text())
+        last_db_id = last_checkins[
+                "response"]["checkins"]["items"][0]["checkin_id"]
+
     pagina = 1
+    checkins = []
+    just_checkins = []
     checkins_log = "checkins.log"
     checkins_file = "checkins.json"
     checkins_access_token: str = cred["checkins_access_token"]
@@ -41,41 +102,45 @@ def get_checkins():
         f"&client_secret={cred['client_secret']}"
     )
 
-    checkins_response = get_checkins_response(checkins_url)
-    checkins_json = checkins_response.json()
-    if checkins_json["meta"]["code"] == 200:
-        with open(f"checkins_{pagina:02d}.log", "w") as clph:
-            json.dump(checkins_json, clph, indent=4)
+    # checkins_ids = [checkin["checkin_id"] for checkin in just_checkins]
+    # if not here, then also must check in the while loop
+    # put in a function ?
+    # if last_db_id in checkins_ids:
+    #     # does this truncate the json or just the local var ?
+    #     # position = checkins_ids.index(last_db_id)
+    #     # just_checkins = just_checkins[:position - 1]
+    #     found_last_db_id = True
 
-        checkins = checkins_json
+    checkins, checkins_json, just_checkins = get_checkins_json(
+        checkins_url, pagina, checkins, just_checkins
+    )
 
-        while checkins_json["response"]["pagination"].get("next_url"):
-            nu = checkins["response"]["pagination"]["next_url"]
-            print(f"next_url: {nu}")
-            pagina += 1
-            print(f"pagina: {pagina:02d}")
-            next_url = (f"{nu}{cred_add}")
-            checkins_response = get_checkins_response(next_url)
-            checkins_json = checkins_response.json()
-            with open(f"checkins_{pagina:02d}.log", "w") as clph:
-                json.dump(checkins_json, clph, indent=4)
-            checkins.update(checkins_json)
-            if nu == "" or nu is None:
-                break
-            if pagina > 2:
-                break
+    if found_last_db_id:
+        return "get_checkins ended on first page"
+        # return "SUCCESS get_checkins ended on first page"
 
-        with open(checkins_log, "w") as clh:
-            clh.write(f"{checkins}")
+    while nu := checkins_json["response"]["pagination"].get("next_url"):
+        pagina += 1
+        next_url = (f"{nu}{cred_add}")
+        time.sleep(5)
 
-        with open(checkins_file, "w") as cfh:
-            json.dump(checkins, cfh, indent=4)
-    else:
-        print(f"non 200 response: {checkins_json['meta']}")
+        checkins, checkins_json, just_checkins = get_checkins_json(
+            next_url, pagina, checkins, just_checkins
+        )
+
+        if nu == "" or nu is None:
+            break
+        if pagina > 7:
+            break
+
+    Path(checkins_log).write_text(f"{checkins}")
+    Path(checkins_file).write_text(json.dumps(checkins, indent=4))
+
+    return "SUCCESS get_checkins ended"
 
 
 def main():
-    get_checkins()
+    print(f"{get_checkins()}")
 
 
 if __name__ == "__main__":
